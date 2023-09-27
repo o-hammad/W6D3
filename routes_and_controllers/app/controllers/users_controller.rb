fb2c5c7a04a38981532336b61b8cd3b4abe0e95a
Balaji <imnotbalaji@gmail.com>
2023-09-27 15:12:06 -0400

    Complete first exercise

--- routes_and_controllers/app/controllers/users_controller.rb
+++ routes_and_controllers/app/controllers/users_controller.rb
@@ -1,5 +1,41 @@
 class UsersController < ApplicationController
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end 
     def index
-        render plain: "I'm in the index action!"
+        render json: User.all
     end
+    
+    def create 
+        user = User.new(user_params)
+        if user.save
+            redirect_to user_url(user)
+        else 
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end 
+    end 
+    def show
+        user = User.find(params[:id])
+        if user 
+             render json: user
+        else 
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end 
+    end 
+    def update
+        user = User.find(params[:id])
+        if user.update(user_params)
+            redirect_to users_url
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end 
+    end 
+    def destroy
+        user = User.find_by(id: params[:id])
+        if user && user.destroy
+            redirect_to users_url
+        else 
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end 
+    end 
 end
--- routes_and_controllers/config/routes.rb
+++ routes_and_controllers/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get 'users', to: 'users#index'
   post 'users', to: 'users#create'
-  get 'users/:id', to: 'users#show'
+  get 'users/:id', to: 'users#show', as: 'user'
   patch 'users/:id', to: 'users#update'
   put 'users/:id', to: 'users#update'
   delete 'users/:id', to: 'users#destroy'
